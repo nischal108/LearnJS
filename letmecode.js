40ef1dadee66360348709203a463e55752837d96
Nischal Bhattarai <nishchalb21@gmail.com>
2023-08-11 12:56:37 +0545

    learningJS

---
+++ letmecode.js
@@ -0,0 +1,84 @@
+// // // // //                         // prints the statement in console
+// // // // //  //console.log("hello world");
+
+// // // // //                         //alert box
+// // // // // // alert("hello");
+
+
+
+// // // // //                     //variables
+// // // // // let age = 56;
+// // // // // console.log(age);
+// // // // // age = age +45;
+// // // // // console.log("you are ",age, " years old");
+
+// // // // // let fname = "nischal";
+// // // // // console.log("hello",fname);
+
+
+// // // // //         //selecting html element by id and changing its content
+// // // // // document.getElementById("p1").innerHTML = "hello " + fname;
+// // // // // document.getElementById("p2").innerHTML = "Your age " + age;
+// // // // // document.getElementById("p3").innerHTML = "Keep Coding  " + fname;
+
+
+
+// //             //operators
+// // // let students = 90;
+// // // // students = students +1;
+// // // students%=7
+// // // students+=1
+// // // console.log(students)
+
+
+
+
+// //             // getting input 
+
+// //                //  easy way 
+// // // let names = window.prompt("what is your name man?");
+
+
+// //               // input from html  
+// //  let usernames;
+// // document.getElementById("mybutton").onclick = function(){
+// //     usernames = document.getElementById("names").value
+// //     console.log("so you are",usernames);
+// //     // change the submit button name to my name
+// //     document.getElementById("mybutton").innerHTML = usernames;
+// // }
+
+
+
+
+
+
+//                 // type conversion
+
+//     // in the below program is the second line where the user input string is converted to integer is removed then if 
+//     // user input 23 it shows 231 but with the lne it shows 24
+
+// let age = window.prompt("enter your age");
+// age = Number(age);
+// age +=1;
+// console.log("badha xa mitra timi tah aba ", age , "lagexau");
+
+// console.log(typeof age);
+
+// let x = 3.145;
+
+// x= Math.round(x);
+// console.log(x);
+
+//wap a program to find th hypeetonuse of a right angled triangle:
+
+// let hypotenuse;
+// let base = window.prompt("enter the value for base")
+// let perpendicuar = window.prompt("enter the value for perpndicular")
+// hypotenuse = Math.sqrt(Math.pow(base,2)+Math.pow(perpendicuar,2));
+// alert(hypotenuse);
+// console.log("the base you entered",base);
+
+//upper program but now th input comes from html boxes
+
+
