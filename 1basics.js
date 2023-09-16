ef681edd8c26c05dd3913e9a8aba7c44d8728a7a
Nischal Bhattarai <99738369+nischal108@users.noreply.github.com>
2023-09-16 10:51:44 +0000

    var,let,const and datatypes

--- 1basics.js
+++ 1basics.js
@@ -1,1 +1,14 @@
 console.log("hello world")
+
+//variables declaration 
+const accountId = 13345;
+let accountEmail = "nishchalb23@gmail.com";
+var accountPassword = "nishchal321";
+city = "jhumka";
+
+
+
+// var is not used generally since it is not braces scoped so we are not allowed to use same variable throughout the code without affecting eachother ;
+/* so we use let instead of var */
+// variable can  be declared without any keyword as well like in example city 
+//if we just declared a variable wthout assigning it an value it has undefined as default value
